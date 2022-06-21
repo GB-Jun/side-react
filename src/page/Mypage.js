7081d7d4bc3ccc7f6781a596f22bc3b319f3aa48
GB-Jun <windgb369@hotmail.com>
2022-06-21 14:35:51 +0800

    Firest react

---
+++ src/page/Mypage.js
@@ -0,0 +1,78 @@
+import "./Mypage.css";
+import { useState } from "react";
+import ColorSelector from "./ColorSelector";
+
+function Mypage() {
+    const [demoBG, setDemoBG] = useState("lightgray");
+    const [colorArray, setColorArray] = useState([
+        "salmon",
+        "lightblue",
+        "red",
+    ]);
+
+    function handleClick(e) {
+        console.log(e.target.style.backgroundColor);
+        setDemoBG(e.target.style.backgroundColor);
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <div
+                        className="color-demo-area"
+                        style={{ backgroundColor: demoBG }}
+                    ></div>
+                </div>
+                <div className="col-6">
+                    <div className="color-selector-area d-flex flex-wrap">
+                        {colorArray.map((color) => {
+                            return (
+                                <ColorSelector
+                                    key={color}
+                                    bgColor={color}
+                                    handleClick={handleClick}
+                                />
+                            );
+                        })}
+
+                        {/* <div className="col-3">
+                            <div
+                                className="color-selector autobg"
+                                style={{ backgroundColor: "salmon" }}
+                                onClick={handleClick}
+                            ></div>
+                        </div>
+                        <div className="col-3">
+                            <div
+                                className="color-selector autobg"
+                                style={{ backgroundColor: "blue" }}
+                                onClick={handleClick}
+                            ></div>
+                        </div>
+                        <div className="col-3">
+                            <div className="color-selector autobg"></div>
+                        </div>
+                        <div className="col-3">
+                            <div className="color-selector autobg"></div>
+                        </div>
+                        <div className="col-3">
+                            <div className="color-selector autobg"></div>
+                        </div>
+                        <div className="col-3">
+                            <div className="color-selector autobg"></div>
+                        </div>
+                        <div className="col-3">
+                            <div className="color-selector autobg"></div>
+                        </div>
+                        <div className="col-3">
+                            <div className="color-selector autobg"></div>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Mypage;
